246b5788747921f68bd07fd056ec3bd493b06ed0
Rosmery Izaguirre <rosmeryiza@Rosmerys-MacBook-Pro.local>
2020-08-05 14:56:30 -0400

    first commit

---
+++ js/tooltip.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    tippy('#virus1', {
+        content: 'On July 10, UF announced its Fall plan (Only 35 percent of undergraduate, graduate and professional course "sections" will be held in person or as a "hybrid").',
+        allowHTML: true,
+        theme: 'light-border',
+      });
+});
