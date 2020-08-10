ea422bad9bfe75eb49f9df629a95ac40fc750e6c
Rosmery Izaguirre <rosmeryiza@Rosmerys-MacBook-Pro.local>
2020-08-09 21:49:52 -0400

    added tooltips to SVG icons

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
     tippy('#virus1', {
-        content: 'On July 10, UF announced its Fall plan (Only 35 percent of undergraduate, graduate and professional course "sections" will be held in person or as a "hybrid").',
+        content: "In Fall, 65 percent of course sections will be completely online and 35 percent will be in person or “hybrid” classes, where instruction is delivered in-person and online, according to an email sent to UF employees and students Friday.<br><br>Read more about UF's Fall plan by clicking on the icon.",
+        allowHTML: true,
+        theme: 'light-border',
+      });
+      tippy('#virus2', {
+        content: 'Students and faculty who opt into COVID-19 testing will be tested at a drive-by site near the Student Health Care Center, according to the Student Health Care Center website.<br><br>Read more questions about UF reopening answered by clicking on the icon.',
         allowHTML: true,
         theme: 'light-border',
       });
